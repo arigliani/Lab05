0477929403d4fe386e1f131b42904fe9bf3d5563
gianni arigliani <gianni arigliani@192.168.1.109>
2017-04-13 21:23:27 +0200

    con mappa, ancoa sbagliatp

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
@@ -2,26 +2,101 @@ package it.polito.tdp.anagrammi.model;
 
 import java.util.*;
 
+
 public class Model {
-	private List<String> elencoParole= new LinkedList<String>();
-	
+	private int i=0;
+	private Map<Integer, String> mappaLettere= new HashMap<Integer, String>();
 	 
-	public List<String> commuta(String parola){
-	//	int fattoriale= calcolaFatoriale(parola);
-	//	int livello=0;
-		char[] arrayCaratteri= parola.toCharArray();
-		char[] prova=new char [parola.length()];
-		int[] vettorePosizione= creaVettore(parola);
-	//	elencoParole.add(parola);
-		//String parziale="";
-	//	this.listaCommutazioni(arrayCaratteri, arrayCaratteri, fattoriale, livello);
-		itera(vettorePosizione, prova, arrayCaratteri,0);
+	public Set<String> commuta(String parola){
+	
+		creaMappa(parola);
 		
-		return elencoParole;
+		Set<String> soluzione= this.risolvi();
+	//	itera(mappaLettere, 0, Set<Pezzo> parziale );// metodo per riempire la lista con iterazione
+	//	itera(vettorePosizione, prova, arrayCaratteri,0);
+		
+		return soluzione;
 		
 	}
 	
-	private int[] creaVettore(String parola) {
+	
+
+	private Set<String> risolvi() {
+		TreeMap<Integer,String> parziale = new TreeMap<Integer,String>();
+		Set<String> elencoParole= new HashSet<String>();
+		scegli (parziale,0, elencoParole);
+		return elencoParole;
+	}
+
+
+
+	private void scegli(TreeMap<Integer,String> parziale, int livello, Set<String> elencoParole) {
+		
+		if(parziale.size()==mappaLettere.size()){
+			//trovata una commutazione
+			// devo salvarla in elencoParole
+			
+			//trasformo parziale in stringa
+			// e controllo se gia esiste
+			String s= trasformaStringa(parziale);
+			elencoParole.add(s);
+			
+		}
+		
+		for(int j=0; j<mappaLettere.size();j++){//parziale linkedList perchemantiene ordine inserimento
+			if(!parziale.containsKey(j)){
+				String temp=(""+i+" "+mappaLettere.get(j));
+				parziale.put(j,temp);
+				i++;
+				
+				scegli(parziale, livello+1, elencoParole);
+				parziale.remove(mappaLettere.get(j));
+				
+			}
+		}
+		
+	}
+
+
+
+	private String trasformaStringa(TreeMap<Integer, String> parziale) {
+		LinkedList<String> lettere=  new LinkedList<String>(parziale.values());
+	    Collections.sort(lettere);
+	    String parola="";
+	    for(String s: lettere ){
+	    	String[] temp=s.split(" ");
+	    	parola+=temp[1];
+	    	
+	    }
+	    return parola.trim();
+		
+	}
+
+
+
+	private void creaMappa(String parola) {
+		char[] caratteri=parola.toCharArray();
+		
+		for(int i=0; i<parola.length();i++){
+			Integer j= i;
+			mappaLettere.put(j, ""+caratteri[i]);
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/*private int[] creaVettore(String parola) {
 		int i= parola.length();
 		int[] vett= new int[i];
 		
@@ -47,113 +122,11 @@ public class Model {
 		}
 	}
 	
-	
-	
-	private String verificaParola(char[] prova, int lunghezza,int livello,String verifica) {
-		if(prova.length==lunghezza){
-			String s=prova.toString();
-			
-			elencoParole.add(s);
-			return "";
-		}
-		else{
-			verifica+=""+livello;
-			return verifica;
-		}
-		
-		
-		
-	}
-
-	/*private void listaCommutazioni(String parola,char[] parziale,char[] arrayCaratteri, int fattoriale, int livello){
-		if(parziale.length()==parola.length()){
-			if(!elencoParole.contains(parziale))
-				elencoParole.add(parziale);
-			  //parziale="";
-		}
-		
-		for(int i=0; i<parola.length();i++){
-			//if(i!=livello) {                        
-			parziale[i]+=arrayCaratteri[i];
-			//System.out.println(livello);
-			//livello=livello+1;
-			listaCommutazioni(parola, parziale,arrayCaratteri, fattoriale, livello+1);
-			//livello=livello-1;
-		   parziale.lastIndexOf("", i);
-		}
-		
-	}*/
-	
-	
-
-/*	private List<String> listaCommutazioni(char[] parziale,char[] arrayCaratteri, int fattoriale, int livello) {// inizializza parziale e aggiungi a lista
-		
-		
-		 while((elencoParole.size()<fattoriale)){
-		   int i=livello;
-		   int j=livello+1;
-		   
-			 String s= this.commutazione(parziale, i, j,livello);
-			// System.out.println(s+" "+i+" "+j+" "+" "+elencoParole.size()+elencoParole);
-			 
-			if(!elencoParole.contains(s)){
-				 elencoParole.add(s);
-				 if((i==livello)&&(j==(livello+1))){
-					 i++;
-					 j++;
-				 }
-				 else{
-					 i--;
-					 j--;
-				 }
-			 }
-			else{
-				livello++;
-				listaCommutazioni(arrayCaratteri, arrayCaratteri,fattoriale, livello);
-			}
-			 
-		 }
-		
-		
-		return elencoParole;
-	}
-
-	private String commutazione(char[] parziale, int i, int j) {
-		char scambioI= parziale[i];
-		char scambioJ=parziale[j];
-		
-		parziale[j]=scambioI;
-		parziale[i]=scambioJ;
-		
-		String s="";
-		
-		for(char c: parziale){
-			s+=c;
-		}
-		
-		
-		return s;
-	}  */
-	
-	
-	
-	
-	
-
-	
-
-/*	private int calcolaFatoriale(String parola) {
-		int lunghezza=parola.length();
-		int fattorialeMetodo=1;
-		
-		for(int k=1; k<=lunghezza; k++ ){
-			fattorialeMetodo= fattorialeMetodo*k;
-		}
-		
-		return fattorialeMetodo;
-	}
-	
 	*/
+	
+	
+
+	
 
 	
 
